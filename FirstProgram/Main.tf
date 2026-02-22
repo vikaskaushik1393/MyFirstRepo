d0375126e511d0e13135165ad6c08b61942b9829
Kaushik <vkaushik@tnsi.com>
2026-02-22 12:24:27 +0530

    new code added

---
+++ FirstProgram/Main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
+}
