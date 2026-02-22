21cb3a7028ef3a6fd0b670d97236297faa22e584
Kaushik <vkaushik@tnsi.com>
2026-02-22 12:33:42 +0530

    new code added_new

--- FirstProgram/Main.tf
+++ FirstProgram/Main.tf
@@ -7,9 +7,11 @@ terraform {
   }
 }
 
-#provider "aws" {
+#new line added
+provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
