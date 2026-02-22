de201948f2d33640780a858d76a2414fe21fdf30
Kaushik <vkaushik@tnsi.com>
2026-02-22 13:25:37 +0530

    new code added_new3

--- FirstProgram/Main.tf
+++ FirstProgram/Main.tf
@@ -11,6 +11,9 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+provider "aws" {
+  region = "us-east-2"
+}
 #nrew line dddfffsdssdfff
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
