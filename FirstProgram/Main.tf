3133312ec2222e193ce461768f0225f2118712d9
vikaskaushik1393 <vikaskaushik1393@gmail.com>
2026-04-27 21:43:49 +0530

    Update Main.tf
    Signed-off-by: vikaskaushik1393 <vikaskaushik1393@gmail.com>

--- FirstProgram/Main.tf
+++ FirstProgram/Main.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  feature {}
+}
+resource "azurerm_resource_group" "exa" {
+  name     = "Jenkin_example-resource-group"
+  location = "East US"
+ }
